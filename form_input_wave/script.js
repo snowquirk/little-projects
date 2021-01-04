66ff1b940343c814abd86dfa2470dcac7069c666
ayaxd <ayaxd0324@gmail.com>
2021-01-04 23:57:47 +1100

    finish form_input_wave

---
+++ form_input_wave/script.js
@@ -0,0 +1,12 @@
+const labels = document.querySelectorAll('.form-control label');
+const inputs = document.querySelectorAll('.form-control input');
+
+labels.forEach((label) => {
+  label.innerHTML = label.innerText
+    .split('')
+    .map(
+      (letter, idx) =>
+        `<span style="transition-delay:${idx * 50}ms">${letter}</span>`
+    )
+    .join('');
+});
